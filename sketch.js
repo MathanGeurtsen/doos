62ddbd9ff1c7de8750582d8ebbbd7cf1d45665ed
Naomi Bras <naomibras@gmail.com>
2019-05-09 10:24:03 +0200

    naomi changes

--- sketch.js
+++ sketch.js
@@ -1,8 +1,3 @@
-function ball1() {
-  fill(900);
-  circle(x,y,d);
-};
-
 
 function setup() {
   //plaats hier de code die maar eenn keer hoeft te worden uitgevoerd
@@ -19,6 +14,14 @@ let h = windowWidth * 18/20;
 let lengthW = 1/3 * windowWidth;
 let lengthH = 1/3 * windowHeight;
 
+
+
+
+  background(000);
+  fill(500);
+  rect(windowWidth* 1/20, windowHeight* 1/20, windowWidth* 18/20, windowHeight* 18/20);
+};
+
 let x1 = 1/6 * (w-x);       let y1 = 1/3 * (h-y);
 let x2 = 23.3/60 * (w-x);  let y2 = 1/3 * (h-y);
 let x3 = 36.6/60* (w-x);    let y3 = 1/3 * (h-y);
@@ -28,14 +31,14 @@ let x6 = 23.3/60* (w-x);    let y6 = 2/3 * (h-y);
 let x7 = 36.6/60* (w-x);    let y7 = 2/3 * (h-y);
 let x8 = 5/6* (w-x);        let y8 = 2/3 * (h-y);
 
-
-  background(000);
-  fill(500);
-  rect(windowWidth* 1/20, windowHeight* 1/20, windowWidth* 18/20, windowHeight* 18/20);
-};
-
 function draw() {
-  ellipse(50, 50, 80, 80);
+  ball1();
+
+  function ball1() {
+    console.log(x1,y1);
+    fill(999);
+    circle(x1,y1,d);
+  };
 
   //plaats hier de code die continue herhaald moet worden.
 };
